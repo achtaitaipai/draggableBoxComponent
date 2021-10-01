ec1b54f441be1822a199e5c257c70fa8c8c03950
Charles Cailleteau <charles.cailleteau@protonmail.com>
2021-10-01 20:43:40 +0200

    correction build name

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
   },
   output: {
     filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'doc'),
+    path: path.resolve(__dirname, 'docs'),
     clean: true,
   },
   devtool: 'inline-source-map',
